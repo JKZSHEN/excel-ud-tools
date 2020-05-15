c111e52b1b7f8653b96dbc7595eb963c252a704f
张在燊 <1043030462@qq.com>
2020-05-15 10:43:38 +0800

    初始化

---
+++ excel-ud-tools/src/main/java/com/jkshen/exceludtools/ExcelUdToolsApplication.java
@@ -0,0 +1,13 @@
+package com.jkshen.exceludtools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelUdToolsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExcelUdToolsApplication.class, args);
+    }
+
+}
---
+++ excel-ud-tools/src/test/java/com/jkshen/exceludtools/ExcelUdToolsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jkshen.exceludtools;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExcelUdToolsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
