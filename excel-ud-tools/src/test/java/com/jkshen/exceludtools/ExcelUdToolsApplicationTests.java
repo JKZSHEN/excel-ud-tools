05597b07b6b8a29ae309519056065f6d5b2b9e02
JKZSHEN <1043030462@qq.com>
2020-05-19 07:42:42 +0800

    临时提交

--- excel-ud-tools/src/test/java/com/jkshen/exceludtools/ExcelUdToolsApplicationTests.java
+++ excel-ud-tools/src/test/java/com/jkshen/exceludtools/ExcelUdToolsApplicationTests.java
@@ -1,12 +1,7 @@
 package com.jkshen.exceludtools;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class ExcelUdToolsApplicationTests {
 
-    @Test
     void contextLoads() {
     }
 
